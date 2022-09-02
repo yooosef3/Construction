3af7bef9719db77c891891c9088a12a848d0a603
Yusef Khedri <yooosef.khedri@gmail.com>
2022-09-02 15:44:13 +0430

    top slider created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,60 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      fontFamily: {
-        'YekanBakh': ['YekanBakh'],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      YekanBakh: ["YekanBakh"],
+    },
+    extend: {
+      animation: {
+        fadeInLeft: "fade-in-left 1s ease-in-out",
+        fadeInRight: "fade-in-right 1s ease-in-out",
+        fadeInTop: "fade-in-top 1s ease-in-out",
+        fadeInBottom: "fade-in-bottom 1s ease-in-out",
       },
-      extend: {
+      keyframes: {
+        "fade-in-left": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        "fade-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0)",
+          },
+        },
+        "fade-in-top": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in-bottom": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(100%)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
